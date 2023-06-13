85431a8981e67f624a93f388495bebb6b8b74ba3
TBolotova <tbolotova48@gmail.com>
2023-06-13 21:21:21 +0300

    added task 3

---
+++ Homework1/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Please enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int excess = number % 2;
+
+if(excess == 0){
+    Console.WriteLine($"Number {number} is even.");
+} else {
+    Console.WriteLine($"Number {number} is odd.");
+}
