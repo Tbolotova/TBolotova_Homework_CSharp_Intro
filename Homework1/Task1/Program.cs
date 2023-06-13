096a95b7f8e27c1f726268ae00129f86f0e5bdec
TBolotova <tbolotova48@gmail.com>
2023-06-13 19:12:37 +0300

    task1

--- Homework1/Task1/Program.cs
+++ Homework1/Task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+//Я прочла условие и примеры, и у меня что-то не совпало) В условии написано, что нужно выдать, 
+//какое больше и какое меньше. В примерах только максимум указан. Сделала по условию
+
+Console.Write("Please enter the first number: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter the second number: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine($"Number {firstNumber} is greater than number {secondNumber}");
+}
+else if(firstNumber < secondNumber)
+{
+    Console.WriteLine($"Number {secondNumber} is greater than number {firstNumber}");
+}
+else
+{
+    Console.WriteLine($"The numbers you entered are equal");
+}
