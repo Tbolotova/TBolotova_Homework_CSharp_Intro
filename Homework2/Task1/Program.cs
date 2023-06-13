2037f47b9c2bb229ef216b1e0490331d9b1376e6
TBolotova <tbolotova48@gmail.com>
2023-06-13 23:16:59 +0300

    added task1

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+// (Проверка на трехзначность при вводе пользователем)
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+bool isThreeDigit = false;
+
+int number = 0;
+
+//Получаем число и проверяем, является ли оно трехзначным
+
+while(isThreeDigit == false){
+    Console.Write("Please enter a three-digit number: ");
+    number = int.Parse(Console.ReadLine()!);
+    int check = number / 100;
+
+    if(check > 0 && check < 10){
+        isThreeDigit = true;
+    }
+}
+
+int secondDigit = number / 10 % 10;
+
+Console.WriteLine($"The second digit in {number} is {secondDigit}.");
+
