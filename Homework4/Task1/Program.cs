6f2d198ecb0560b0f59e5908f464213be10119d1
TBolotova <tbolotova48@gmail.com>
2023-06-21 22:42:19 +0300

    Added task1 and intitialized tasks 2 and 3.

---
+++ Homework4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Please enter number A: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter number B: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Number {firstNumber} to power {secondNumber} equals {ToPow(firstNumber, secondNumber)}");
+
+int ToPow(int a, int b)
+{
+    int result = a;
+    for (int i = 2; i <= b; i++)
+    {
+        result = result * a;
+    }
+
+    return result;
+}
