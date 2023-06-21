6642f5d6de69238974ef7d0e6d28fcdd8d877417
TBolotova <tbolotova48@gmail.com>
2023-06-21 22:59:36 +0300

    Added task2

--- Homework4/Task2/Program.cs
+++ Homework4/Task2/Program.cs
@@ -3,3 +3,21 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Please enter a number: ");
+
+string numberString = Console.ReadLine()!;
+
+int length = numberString.Length;
+
+Console.WriteLine($"The sum of the digits of {numberString} equals {SumOfDigits(numberString, length)}");
+
+int SumOfDigits(string numberString, int length)
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += Convert.ToInt32(Char.GetNumericValue(numberString[i]));
+    }
+
+    return sum;
+}
