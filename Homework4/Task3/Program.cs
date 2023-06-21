ff1fe52aaeaa014923b24192cb893a91774202ce
TBolotova <tbolotova48@gmail.com>
2023-06-21 23:12:37 +0300

    Added task3

--- Homework4/Task3/Program.cs
+++ Homework4/Task3/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int arrSize = 8;
+int[] array = new int[arrSize];
+FillArray(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 1000);
+    }
+}
+
